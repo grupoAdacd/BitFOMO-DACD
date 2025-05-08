aad1f4df6002a71f7dabbe92c57a84a58a182100
d4rk <herreragp334@gmail.com>
2025-05-08 18:58:10 +0100

    feat(transformer/parser) create JsonParserProvider interface, allowing to use JSONObject or JSONArray properties keeping the Dependency inversion and Liskov principles.

--- binance-feeder/src/main/java/com/bitfomo/transformer/parser/JsonParserProvider.java
+++ binance-feeder/src/main/java/com/bitfomo/transformer/parser/JsonParserProvider.java
@@ -2,8 +2,6 @@ package com.bitfomo.transformer.parser;
 import com.bitfomo.domain.model.CandlestickData;
 import org.json.*;
 
-import java.util.ArrayList;
-
 public interface JsonParserProvider {
     JSONObject parseObject() throws JSONException;
     JSONArray parseArray() throws JSONException;
