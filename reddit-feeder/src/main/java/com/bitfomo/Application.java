d48e81fbe2c07d6283c7fd158c767c7734f50b8f
juand4569 <juand24601@gmail.com>
2025-05-09 18:29:49 +0200

    fix(clean-reddit): erase using formatter and change java 24 to 21

--- reddit-feeder/src/main/java/com/bitfomo/Application.java
+++ reddit-feeder/src/main/java/com/bitfomo/Application.java
@@ -12,8 +12,6 @@ import com.bitfomo.domain.PostRepositoryPort;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -21,16 +19,15 @@ import java.util.concurrent.TimeUnit;
 
 public class Application {
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter .ofPattern("MM/dd/yyyy hh:mm:ss a")
-            .withZone(ZoneId.systemDefault());
-    private static final List<String> SUBREDDITS = List.of("Bitcoin", "CryptoCurrency", "CryptoMarkets");
+    private static final List<String> SUBREDDITS =
+            List.of("Bitcoin", "CryptoCurrency", "CryptoMarkets");
     private static final int POST_LIMIT = 50;
     private static final int INITIAL_DELAY_SECONDS = 0;
     private static final int PERIOD_MINUTES = 5;
 
     public static void main(String[] args) {
         if (args.length < 4) {
-            System.err.println("Usage: Did not find <userAgent> <jdbcUrl> <brokerUrl> <queueName>");
+            System.err.println("Usage: java -jar reddit-feeder.jar <userAgent> <jdbcUrl> <brokerUrl> <queueName>");
             System.exit(1);
         }
 
@@ -43,8 +40,8 @@ public class Application {
         PostRepositoryPort postRepo = new JdbcPostRepository(jdbcUrl);
         EventPublisherPort publisher = new ActiveMqEventPublisher(brokerUrl, queueName);
 
-        FetchRedditPostsUseCase fetchPosts = new FetchRedditPostsUseCaseImpl( redditApi, postRepo,
-                SUBREDDITS, POST_LIMIT, publisher);
+        FetchRedditPostsUseCase fetchPosts = new FetchRedditPostsUseCaseImpl(
+                redditApi, postRepo, SUBREDDITS, POST_LIMIT, publisher);
 
         ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
         scheduler.scheduleAtFixedRate(() -> {
