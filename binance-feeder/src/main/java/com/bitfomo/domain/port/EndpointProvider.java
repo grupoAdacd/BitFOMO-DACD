7cea310bec56cb716670905cf46518a32fc305eb
juand4569 <juand24601@gmail.com>
2025-05-11 00:34:48 +0200

    fix(packages): erase complexity of packages

--- binance-feeder/src/main/java/com/bitfomo/domain/port/EndpointProvider.java
+++
@@ -1,5 +0,0 @@
-package com.bitfomo.domain.port;
-
-public interface EndpointProvider {
-    String createApiUrl();
-}
--- binance-feeder/src/main/java/com/bitfomo/domain/port/ExchangeDataProvider.java
+++
@@ -1,5 +0,0 @@
-package com.bitfomo.domain.port;
-
-public interface ExchangeDataProvider {
-    String fetchWhenInformation(long startDateTime, long endDateTime);
-}
