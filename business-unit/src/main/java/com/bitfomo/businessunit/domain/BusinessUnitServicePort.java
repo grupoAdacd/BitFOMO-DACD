cdf4385fcfcc144d95c5ad73863baa26c8d128c3
juand4569 <juand24601@gmail.com>
2025-05-10 18:30:20 +0200

    feature(port): create a port for the business logic (calculate metrics and generate recommendations)

---
+++ business-unit/src/main/java/com/bitfomo/businessunit/domain/BusinessUnitServicePort.java
@@ -0,0 +1,7 @@
+package com.bitfomo.businessunit.domain;
+
+public interface BusinessUnitServicePort {
+    double calculateAverageSentiment(String subreddit, String startTime, String endTime);
+    double getAveragePrice(String symbol, String startTime, String endTime);
+    String generateRecommendation(String subreddit, String symbol, String startTime, String endTime);
+}
