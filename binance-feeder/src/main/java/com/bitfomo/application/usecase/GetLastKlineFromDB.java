0af2266f33d62717b9d2aed96d83080d59c5f315
d4rk <herreragp334@gmail.com>
2025-05-10 22:09:55 +0100

    feat(application/usecase) create GetLastKlineFromDB class to stract that last date inserted into db.

---
+++ binance-feeder/src/main/java/com/bitfomo/application/usecase/GetLastKlineFromDB.java
@@ -0,0 +1,31 @@
+package com.bitfomo.application.usecase;
+
+import com.bitfomo.adapters.persistence.manager.DatabaseManager;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class GetLastKlineFromDB {
+    public long getLastKline() {
+        long lastTime = 0;
+        try (Connection conn = DriverManager.getConnection(DatabaseManager.getDatabaseUrl());
+             Statement stmt = conn.createStatement()) {
+            String sql = "SELECT MAX(Kline_Close_Time) as last_time FROM BinanceEvents";
+            ResultSet rs = stmt.executeQuery(sql);
+            if (rs.next() && rs.getString("last_time") != null) {
+                String dateStr = rs.getString("last_time");
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                Date date = sdf.parse(dateStr);
+                lastTime = date.getTime();
+            }
+
+        } catch (Exception e) {
+            System.err.println("Error when trynna grab LastKlineTime parameter: " + e.getMessage());
+        }
+        return lastTime;
+    }
+}
