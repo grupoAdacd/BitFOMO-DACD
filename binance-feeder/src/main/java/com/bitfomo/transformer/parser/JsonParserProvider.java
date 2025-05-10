5f930d127d2feec0bf98ef73d3765ca7ac530b85
d4rk <herreragp334@gmail.com>
2025-05-10 22:11:23 +0100

    feat(transformer/parser) refactoring and formating JsonParserProvider interface and JsonDataParser class.

--- binance-feeder/src/main/java/com/bitfomo/transformer/parser/JsonParserProvider.java
+++ binance-feeder/src/main/java/com/bitfomo/transformer/parser/JsonParserProvider.java
@@ -1,9 +1,7 @@
 package com.bitfomo.transformer.parser;
-import com.bitfomo.domain.model.CandlestickData;
 import org.json.*;
 
 public interface JsonParserProvider {
     JSONObject parseObject() throws JSONException;
     JSONArray parseArray() throws JSONException;
-    JSONArray serializer(CandlestickData candleStick) throws JSONException;
 }
