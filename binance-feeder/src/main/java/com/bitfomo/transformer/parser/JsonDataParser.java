68cd82c0e5f356ffe942f7087ee86b0064a15ab0
d4rk <herreragp334@gmail.com>
2025-05-10 22:11:23 +0100

    feat(transformer/parser) refactoring and formating JsonParserProvider interface and JsonDataParser class.

--- binance-feeder/src/main/java/com/bitfomo/transformer/parser/JsonDataParser.java
+++ binance-feeder/src/main/java/com/bitfomo/transformer/parser/JsonDataParser.java
@@ -1,11 +1,8 @@
 package com.bitfomo.transformer.parser;
 
-import com.bitfomo.domain.model.CandlestickData;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.util.ArrayList;
 
 public class JsonDataParser implements JsonParserProvider {
     private final String jsonString;
@@ -22,21 +19,5 @@ public class JsonDataParser implements JsonParserProvider {
     @Override
     public JSONArray parseArray() throws JSONException {
         return new JSONArray(this.jsonString);
-    }
-
-    @Override
-    public JSONArray serializer(CandlestickData candleStick) throws JSONException {
-        ArrayList<Object> candleStickArray = new ArrayList<>();
-        candleStickArray.add("Kline Open Time:"+ candleStick.getKlineOpenTime());
-        candleStickArray.add("Open Price: " + candleStick.getOpenPrice());
-        candleStickArray.add("High Price: "+ candleStick.getHighPrice());
-        candleStickArray.add("Low Price: "+ candleStick.getLowPrice());
-        candleStickArray.add("Close Price: "+ candleStick.getClosePrice());
-        candleStickArray.add("Quote Asset Volume: "+ candleStick.getQuoteAssetVolume());
-        candleStickArray.add("Volume: "+ candleStick.getVolume());
-        candleStickArray.add("Kline Close Time: "+ candleStick.getKlineCloseTime());
-        candleStickArray.add("Number of Trades: "+ candleStick.getNumberOfTrades());
-
-        return new JSONArray(candleStickArray);
     }
 }
