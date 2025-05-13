2f457b61fbeb2e5a103d0585a7e207c68da328e8
d4rk <herreragp334@gmail.com>
2025-05-13 15:15:16 +0100

    fix(com.bitfomo) fix Main of business-unit so now it functions with the AsciiArtBanner class.

--- business-unit/src/main/java/com/bitfomo/Main.java
+++ business-unit/src/main/java/com/bitfomo/Main.java
@@ -1,3 +1,4 @@
+
 package com.bitfomo;
 
 import com.bitfomo.businessunit.adapters.ActiveMQMessageConsumer;
@@ -8,6 +9,7 @@ import com.bitfomo.businessunit.domain.BusinessUnitServicePort;
 import com.bitfomo.businessunit.domain.DatamartPort;
 import com.bitfomo.businessunit.domain.EventStoreReaderPort;
 import com.bitfomo.businessunit.domain.MessageConsumerPort;
+import com.bitfomo.businessunit.ui.AsciiArtBanner;
 import com.bitfomo.businessunit.ui.CliUserInterface;
 
 import java.util.List;
@@ -18,24 +20,25 @@ public class Main {
             System.err.println("Usage: java -jar business-unit.jar <brokerUrl> <csvPath>");
             System.exit(1);
         }
-
         String brokerUrl = args[0];
         String csvPath = args[1];
-
+        AsciiArtBanner.printBanner("BitFOMO");
         DatamartPort datamartPort = new CsvDatamart(csvPath);
         EventStoreReaderPort eventStoreReader = new FileEventStoreReader();
-
         BusinessUnitServicePort businessUnitService = new BusinessUnitService(datamartPort, eventStoreReader);
-
         MessageConsumerPort messageConsumer = new ActiveMQMessageConsumer(
                 List.of("RedditPost", "CryptoPrice"),
                 brokerUrl,
                 datamartPort
         );
 
-        new Thread(messageConsumer::startConsuming).start();
+        Thread consumerThread = new Thread(messageConsumer::startConsuming);
+        consumerThread.setDaemon(true);
+        consumerThread.start();
 
-        // Inicia la CLI
+        System.out.println("Connecting to broker: " + brokerUrl);
+        System.out.println("Using Datamart: " + csvPath);
+        System.out.println("Initializing user interface...");
         CliUserInterface cli = new CliUserInterface(businessUnitService);
         cli.start();
     }
