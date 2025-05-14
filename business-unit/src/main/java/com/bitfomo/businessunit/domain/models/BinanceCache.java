758639a64c2d176be623ed9cf445efd203e13229
d4rk <herreragp334@gmail.com>
2025-05-14 18:14:38 +0100

    feat(domain) create BinanceCache class which implements CacheUtil.

---
+++ business-unit/src/main/java/com/bitfomo/businessunit/domain/models/BinanceCache.java
@@ -0,0 +1,38 @@
+package com.bitfomo.businessunit.domain.models;
+import com.bitfomo.businessunit.domain.CacheUtil;
+import com.bitfomo.domain.Candlestick;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class BinanceCache implements CacheUtil {
+    private Map<String, Candlestick> financialCache;
+    private final List<String> keyList = Arrays.asList("ts", "openPrice", "closePrice");
+
+    @Override
+    public void insert(){
+        BinanceCache binanceCache = new BinanceCache();
+        Map<String, Candlestick> result=null;
+
+
+        binanceCache.setFinancialCache(result);//setter con el diccionario bien
+    }
+
+    @Override
+    public Candlestick extract(String fileName){// cada line es un candlestick
+        return null;
+    }
+
+    public Map<String, Candlestick> getFinancialCache() {
+        return financialCache;
+    }
+
+    public void setFinancialCache(Map<String, Candlestick> financialCache) {
+        this.financialCache = financialCache;
+    }
+
+    public List<String> getKeyList() {
+        return keyList;
+    }
+}
