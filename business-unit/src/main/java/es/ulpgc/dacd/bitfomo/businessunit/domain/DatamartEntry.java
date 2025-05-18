44295babfaba1d90398396df00fdec9964391eb3
juand4569 <juand24601@gmail.com>
2025-05-18 14:02:07 +0200

    feat(busines-unit): business-unit first version

--- business-unit/src/main/java/es/ulpgc/dacd/bitfomo/businessunit/domain/DatamartEntry.java
+++ business-unit/src/main/java/es/ulpgc/dacd/bitfomo/businessunit/domain/DatamartEntry.java
@@ -1,5 +1,3 @@
 package es.ulpgc.dacd.bitfomo.businessunit.domain;
 
-public record RedditEvent(String id, long ts, String text) {}
-public record BinanceEvent(long ts, double openPrice, double closePrice) {}
 public record DatamartEntry(long ts, double sentiment, double openPrice, double closePrice) {}
